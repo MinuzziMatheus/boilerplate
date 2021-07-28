d9a20611da2a64afe57dfd705fdb05910fb4bb01
matheus <matheus.minuzzi@universo.univates.br>
2021-07-27 22:25:48 -0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
